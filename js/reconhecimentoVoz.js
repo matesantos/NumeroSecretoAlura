e67f5bbe1f112daa1d4d14b973376d51b27b6324
Mateus Santos <matesantos@hotmail.com>
2023-04-19 00:45:10 -0300

    reconhecimento de voz

--- js/reconhecimentoVoz.js
+++ js/reconhecimentoVoz.js
@@ -1,3 +1,2 @@
-const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
-const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
